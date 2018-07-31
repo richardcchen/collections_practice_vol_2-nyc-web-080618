730f9dd33c0bdd61028d19921411472086a324ed
Richard Chen <richard.c.chen@gmail.com>
2018-07-31 17:40:24 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,20 +39,30 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-  rethash = {}
-  array.each do |x|
-    counter = array.count(x)
-    x.each do |key, value|
-      rethash.merge!({key => value, :count => counter})
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
     end
-  end 
-  rethash = {:name => "blake", :count => 2}, {:name => "ashley", :count => 1}
-  rethash
+  end.uniq
 end
 
-def merge_data(data1, data2)
-  retarray = data1
-  retarray = {:first_name => "blake", :awesomeness => 10, :height => "74", :last_name => "johnson"}, {:first_name => "ashley", :awesomeness => 9, :height => 60, :last_name => "dubs"}
+def merge_data(keys, values)
+  container = []
+  keys.each do |person_name|
+    name = person_name[:first_name]
+    values.each do |person_data|
+      if person_data[name]
+        merged_person = person_data[name]
+        merged_person[:first_name] = name
+        container << merged_person
+      end
+    end
+  end
+  container
 end
 
 def find_cool(data)
@@ -61,5 +71,16 @@ def find_cool(data)
 end
 
 def organize_schools(data)
-  {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"], "SF"=>["dev boot camp", "Hack Reactor"], "Chicago"=>["dev boot camp chicago"]}
+  orgschools = {}
+  data.each do |school, locationhash|
+    location = locationhash[:location]
+    if orgschools[location]
+      orgschools[location] << school
+    else
+      orgschools[location] = []
+      orgschools[location] << school
+    end
+  end
+  
+  orgschools
 end 
